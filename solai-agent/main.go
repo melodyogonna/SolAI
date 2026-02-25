680dc6ff53c2cdfffb50fac6d8e62b238a33640b
Melody Daniel <melodyogonna@gmail.com>
2026-02-25 23:20:41 +0100

    [solai-agent] Scaffolding agent

---
+++ solai-agent/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/tmc/langchaingo/agents"
+	"github.com/tmc/langchaingo/chains"
+	"github.com/tmc/langchaingo/llms/googleai"
+	"github.com/tmc/langchaingo/tools"
+)
+
+func main() {
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Fatal("API key needs to be specified.")
+	}
+	systemPromptLocation := os.Getenv("SYSTEM_PROMPT")
+	if systemPromptLocation == "" {
+		log.Fatal("system prompt not passed, exiting.")
+	}
+
+	file, err := os.Open(systemPromptLocation)
+	if err != nil {
+		log.Fatalf("could not open system prompt at location %s - err %s", systemPromptLocation, err)
+	}
+	defer file.Close()
+
+	ctx := context.Background()
+	llm, err := googleai.New(ctx, googleai.WithAPIKey(apiKey), googleai.WithDefaultModel("gemini-2.5-pro"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	prompt, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("unable to read prompt due to err - %s", err)
+	}
+
+	agent := agents.NewOneShotAgent(llm, []tools.Tool{}, agents.WithMaxIterations(3))
+	executor := agents.NewExecutor(agent)
+	answer, err := chains.Run(ctx, executor, string(prompt))
+	if err != nil {
+		log.Print(err)
+	}
+
+	fmt.Print(answer)
+}
