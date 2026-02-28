4bac635058d847cd2f58cb093b650daa43e16201
Melody Daniel <melodyogonna@gmail.com>
2026-02-28 20:51:01 +0100

    Move things around a bit

--- solai-agent/wallet/wallet.go
+++ solai-agent/wallet/wallet.go
@@ -2,6 +2,7 @@ package wallet
 
 import (
 	"crypto/ed25519"
+	"fmt"
 
 	"github.com/tyler-smith/go-bip39"
 )
@@ -25,6 +26,26 @@ func seedFromPhrase(phrase string) ([]byte, error) {
 func createWalletFromSeed(seed []byte) (ed25519.PrivateKey, ed25519.PublicKey) {
 	privateKey := ed25519.NewKeyFromSeed(seed)
 	return privateKey, privateKey.Public().(ed25519.PublicKey)
+}
+
+func CreateWallet(seed []byte) (SolKeyPair, error) {
+	if seed != nil {
+		prv, pub := createWalletFromSeed(seed)
+		return SolKeyPair{privateKey: prv, publicKey: pub}, nil
+	}
+
+	mnemonic, err := generateSeedPhrase()
+	if err != nil {
+		return SolKeyPair{}, fmt.Errorf("unable to generate key phrase %w", err)
+	}
+
+	seed, err = seedFromPhrase(mnemonic)
+	if err != nil {
+		return SolKeyPair{}, fmt.Errorf("unable to generate seed from phrase %w", err)
+	}
+
+	prv, pub := createWalletFromSeed(seed)
+	return SolKeyPair{privateKey: prv, publicKey: pub}, nil
 }
 
 func SignTransaction() {}
