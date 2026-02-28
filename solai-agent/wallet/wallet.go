936fea5d2354599d101ec775c130bd3018e35081
Melody Daniel <melodyogonna@gmail.com>
2026-02-28 20:03:11 +0100

    feat: Implement solai-agent wallet

---
+++ solai-agent/wallet/wallet.go
@@ -0,0 +1,10 @@
+package wallet
+
+// AI! make this function generate a BIP-39 seed phrase for use with solana wallets
+func generateSeedPhrase() {}
+
+func CreateWallet() {}
+
+func SignTransaction() {}
+
+func VerifyTransaction() {}
